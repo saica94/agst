52ed233b01c86e0c00b9c3ba7223e94230e19c37
saica94 <105745241+saica94@users.noreply.github.com>
2022-12-30 06:36:17 +0900

    version up v1.1.1

--- agst.ps1
+++ agst.ps1
@@ -1,4 +1,8 @@
-﻿# Auto Gaming Setting Tool Ver.GUI-1.1.0
+﻿# Auto Gaming Setting Tool Ver.GUI-1.1.1
+
+# 管理者権限の確認
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$bool_admin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
 # アセンブリの読み込み
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
@@ -6,20 +10,44 @@
 
 # 先に関数定義をしたくないので
 $SKIP=$true;iex ((gc $MyInvocation.InvocationName|%{
-    if ($SKIP -and ($_ -notmatch "^####FuncDef\s*$")){return}
+    if($SKIP -and ($_ -notmatch "^####FuncDef\s*$")){return}
     $SKIP=$false
     $_
 })-join "`r`n")
 
 # global変数の定義
  # マウス加速関連
-$Mouse_acc_path = "HKCU:\Control Panel\Mouse"
-$Mouse_acc_key1 = "MouseSpeed"
-$Mouse_acc_key2 = "MouseThreshold1"
-$Mouse_acc_key3 = "MouseThreshold2"
+$Mouse_acc_Path = "HKCU:\Control Panel\Mouse"
+$Mouse_acc_Key1 = "MouseSpeed"
+$Mouse_acc_Key2 = "MouseThreshold1"
+$Mouse_acc_Key3 = "MouseThreshold2"
  # タスクバー関連
-$Taskbar_size_path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-$Taskbar_size_key1 = "TaskbarSi"
+$TaskBar_Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+$TaskBar_Size_Key1 = "TaskbarSi"
+$TaskIcon_Position_Key1 = "TaskbarAl"
+$TaskBar_ClassicMode_Key1 = "Start_ShowClassicMode"
+$TaskBar_Search_Disabled_Path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search"
+$TaskBar_Search_Disabled_Key1 = "SearchboxTaskbarMode"
+$TaskBar_Widged_Key1 = "Taskbarda"
+ # XBox Game bar 関連
+$XBox_Game_bar_Path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR"
+$XBox_Game_bar_Key1 = "AppCaptureEnabled"
+ # 固定キー関連
+$StickyKeys_Path = "HKCU:\Control Panel\Accessibility\StickyKeys"
+$StickyKeys_Key1 = "Flags"
+ # Windows 広告関連
+$AdvertisingInfo_Path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo"
+$AdvertisingInfo_Key1 = "Enabled"
+$SyncProviderNotif_Key1 = "ShowSyncProviderNotifications"
+$ContentDeliver_Path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
+$ContentDeliver_Key1 = "SubscribedContent-338388Enabled"
+$ContentDeliver_Key2 = "SubscribedContent-310093Enabled"
+$ContentDeliver_Key3 = "SubscribedContent-338389Enabled"
+$LockScreenOverlay_Key1 = "RotatingLockScreenOverlayEnabled"
+$UserProfileEngagement_Path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\UserProfileEngagement"
+$UserProfileEngagement_Key1 = "ScoobeSystemSettingEnabled"
+$Privacy_Path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Privacy"
+$Privacy_Key1 = "TailoredExperiencesWithDiagnosticDataEnabled"
 
 # フォームの設定
 $Form = New-Object System.Windows.Forms.Form
@@ -28,7 +56,6 @@ $Form.Text = "Auto Gaming Setting Tool Ver1.1.0"
 $Form.MaximizeBox = $false
 $Form.MinimizeBox = $false
 $Form.FormBorderStyle = "Fixed3D"
-#$Form.ShowInTaskbar = $false
 $Form.ShowIcon = $false
 
 # ボタンの設定
@@ -59,48 +86,55 @@ $OFF.Text = "マウス加速-OFF"
 
  # タスクバー変更
  # グループの設定
-$Taskbar_size = New-Object System.Windows.Forms.GroupBox
-$Taskbar_size.Location = New-Object System.Drawing.Point(185,5)
-$Taskbar_size.Size = New-Object System.Drawing.Size(180,100)
-$Taskbar_size.Text = "タスクバー設定"
+$TaskBar_Size = New-Object System.Windows.Forms.GroupBox
+$TaskBar_Size.Location = New-Object System.Drawing.Point(185,5)
+$TaskBar_Size.Size = New-Object System.Drawing.Size(180,100)
+$TaskBar_Size.Text = "タスクバー設定"
  # ラジオボタン配置
-$LAR = New-Object System.Windows.Forms.RadioButton
-$LAR.Location = New-Object System.Drawing.Point(10,15)
-$LAR.Size = New-Object System.Drawing.Size(120,30)
-$LAR.Text = "タスクバーサイズ：大"
-$MID = New-Object System.Windows.Forms.RadioButton
-$MID.Location = New-Object System.Drawing.Point(10,40)
-$MID.Size = New-Object System.Drawing.Size(120,30)
-$MID.Text = "タスクバーサイズ：中"
-$MID.Checked = $True
-$SMA = New-Object System.Windows.Forms.RadioButton
-$SMA.Location = New-Object System.Drawing.Point(10,65)
-$SMA.Size = New-Object System.Drawing.Size(120,30)
-$SMA.Text = "タスクバーサイズ：小"
+$LARGE = New-Object System.Windows.Forms.RadioButton
+$LARGE.Location = New-Object System.Drawing.Point(10,15)
+$LARGE.Size = New-Object System.Drawing.Size(120,30)
+$LARGE.Text = "タスクバーサイズ：大"
+$MEDIUM = New-Object System.Windows.Forms.RadioButton
+$MEDIUM.Location = New-Object System.Drawing.Point(10,40)
+$MEDIUM.Size = New-Object System.Drawing.Size(120,30)
+$MEDIUM.Text = "タスクバーサイズ：中"
+$MEDIUM.Checked = $True
+$SMALL = New-Object System.Windows.Forms.RadioButton
+$SMALL.Location = New-Object System.Drawing.Point(10,65)
+$SMALL.Size = New-Object System.Drawing.Size(120,30)
+$SMALL.Text = "タスクバーサイズ：小"
 
 # グループにラジオボタンを入れる
 $Mouse_acc.Controls.AddRange(@($ON,$OFF))
-$Taskbar_size.Controls.AddRange(@($LAR,$MID,$SMA))
+$TaskBar_Size.Controls.AddRange(@($LARGE,$MEDIUM,$SMALL))
 $Form.Controls.Add($Mouse_acc)
-$Form.Controls.Add($Taskbar_size)
-# $Form.TopMost
+$Form.Controls.Add($TaskBar_Size)
 
 # テキストボックスの設定
-$TextBox = New-Object System.Windows.Forms.Textbox
+$TextBox = New-Object System.Windows.Forms.RichTextbox
 $TextBox.Location = New-Object System.Drawing.Point(5,140)
 $TextBox.Size = New-Object System.Drawing.Size(360,120)
 $TextBox.Multiline = $True
 $TextBox.ScrollBars = [Windows.Forms.ScrollBars]::Vertical
 $Form.Controls.Add($TextBox)
+$TextBox.SelectionColor = "Red"
+if($bool_admin){
+    $TextBox.AppendText("管理者権限で実行されています。`r`n")
+} else {
+    $TextBox.AppendText("管理者権限で起動されていない為、一部の設定を変更できません。`r`n")
+    $TextBox.SelectionColor = "Red"
+    $TextBox.AppendText("全ての設定を正しく変更する為に、管理者権限で実行しなおして下さい。`r`n")
+}
 
 # フォームの表示
 $Form.Icon = $MyIcon
 $Form.Add_Shown({$Form.Activate()})
-$Form.TopMost = $True
 $DialogResult = $Form.ShowDialog()
 
 ####FuncDef
 function Btn_Click(){
+    $TextBox.SelectionColor = "Black"
     # Google ChromeのDL＆インストール
     $TextBox.AppendText("Google Chrome のダウンロードを行います。`r`n")
     $Path = $env:TEMP
@@ -114,27 +148,58 @@ function Btn_Click(){
     if($OFF.Checked){
         $TextBox.AppendText("マウス加速を切ります。`r`n")
         # キーを更新する
-        Set-ItemProperty $Mouse_acc_path -name $Mouse_acc_key1 -value "0" -Force
-        Set-ItemProperty $Mouse_acc_path -name $Mouse_acc_key2 -value "0" -Force
-        Set-ItemProperty $Mouse_acc_path -name $Mouse_acc_key3 -value "0" -Force
+        Set-ItemProperty $Mouse_acc_Path -name $Mouse_acc_Key1 -Value "0" -Force
+        Set-ItemProperty $Mouse_acc_Path -name $Mouse_acc_Key2 -Value "0" -Force
+        Set-ItemProperty $Mouse_acc_Path -name $Mouse_acc_Key3 -Value "0" -Force
     }
 
-    if($LAR.Checked){
+    if($LARGE.Checked){
         $TextBox.AppendText("タスクバーのサイズを`"大`"に変更します。`r`n")
-        Set-ItemProperty $Taskbar_size_path -Name $Taskbar_size_key1 -Value "2" -Force
-    } elseif($MID.Checked){
+        Set-ItemProperty $TaskBar_Path -Name $TaskBar_Size_Key1 -Value "2" -Force
+    } elseif($MEDIUM.Checked){
         $TextBox.AppendText("タスクバーのサイズを`"中`"に変更します。`r`n")
-        Set-ItemProperty $Taskbar_size_path -Name $Taskbar_size_key1 -Value "1" -Force
+        Set-ItemProperty $TaskBar_Path -Name $TaskBar_Size_Key1 -Value "1" -Force
     } else {
         $TextBox.AppendText("タスクバーのサイズを`"小`"に変更します。`r`n")
-        Set-ItemProperty $Taskbar_size_path -Name $Taskbar_size_key1 -Value "0" -Force
+        Set-ItemProperty $TaskBar_Path -Name $TaskBar_Size_Key1 -Value "0" -Force
     }
+
+    # Windows 10のようなスタートメニューに変更する
+    $TextBox.AppendText("Windows10仕様のスタートメニューに変更します。`r`n")
+    Set-ItemProperty $TaskBar_Path -Name $TaskBar_ClassicMode_Key1 -Value "0" -Force
+    $TextBox.AppendText("タスクバーの検索アイコンを非表示にします。`r`n")
+    Set-ItemProperty $TaskBar_Search_Disabled_Path -Name $TaskBar_Search_Disabled_Key1 -Value "0" -Force
+    $TextBox.AppendText("タスクバーのウィジェットアイコンを非表示にします。`r`n")
+    Set-ItemProperty $TaskBar_Path -Name $TaskBar_Widged_Key1 -Value "0" -Force
+    # XBox Game bar の無効化
+    $TextBox.AppendText("XBox Game bar を無効化します。`r`n")
+    Set-ItemProperty $XBox_Game_bar_Path -Name $XBox_Game_bar_Key1 -Value "0" -Force
+
+    # 固定キーの無効化
+    $TextBox.AppendText("固定キー機能を無効化します。`r`n")
+    Set-ItemProperty $StickyKeys_Path -Name $StickyKeys_Key1 -Value "0" -Force
+
+    # Windows 広告の無効化
+    $TextBox.AppendText("Windows 広告を無効化します。`r`n")
+    Set-ItemProperty $AdvertisingInfo_Path -Name $AdvertisingInfo_Key1 -Value "0" -Force
+    Set-ItemProperty $AdvertisingInfo_Path -Name $SyncProviderNotif_Key1 -Value "0" -Force
+    Set-ItemProperty $ContentDeliver_Path -Name $ContentDeliver_Key1 -Value "0" -Force
+    Set-ItemProperty $ContentDeliver_Path -Name $ContentDeliver_Key2 -Value "0" -Force
+    Set-ItemProperty $ContentDeliver_Path -Name $ContentDeliver_Key3 -Value "0" -Force
+    Set-ItemProperty $ContentDeliver_Path -Name $LockScreenOverlay_Key1 -Value "0" -Force
+    Set-ItemProperty $UserProfileEngagement_Path -Name $UserProfileEngagement_Key1 -Value "0" -Force
+    Set-ItemProperty $Privacy_Path -Name $Privacy_Key1 -Value "0" -Force
+
     $TextBox.AppendText("タスクバーの設定を反映させる為にexplorer.exeを再起動します。`r`n")
     $nid = (Get-Process explorer).id
 
     Stop-Process -Id $nid
     Wait-Process -Id $nid
     Start-Process "explorer.exe"
+
+    # タスクバーの位置を左揃えにする
+    $TextBox.AppendText("タスクアイコンを左揃えにします。`r`n")
+    Set-ItemProperty $TaskBar_Path -Name $TaskIcon_Position_Key1 -Value "0" -Force
 
     $TextBox.AppendText("電源プランの変更をします。`r`n")
     $TextBox.AppendText("一部のRyzen CPUではこの処理はスキップされます。`r`n")
